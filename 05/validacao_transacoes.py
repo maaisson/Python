984d1c2cd370b4cb953056850a5362e5031cff79
Maisson Fernandes <maisson.fernandes@integrativa.com.br>
2025-02-14 16:59:31 -0300

    aaaaa

--- 05/validacao_transacoes.py
+++ 05/validacao_transacoes.py
@@ -12,16 +12,17 @@ conn = get_db_connection()
 df.to_sql('transactions', conn, if_exists='replace', index=False, dtype={'amount': Float})
 
 with conn.connect() as connection:
-    t = text('SELECT category as categoria, status, sum(amount) as soma FROM transactions Group By status, categoria ORDER BY categoria, status;')    
+    t = text('SELECT currency as moeda, category as categoria, status, sum(amount) as soma FROM transactions Group By moeda, status, categoria ORDER BY categoria, status;')    
     result = connection.execute(t)	
     rows = result.fetchall()
     columns = result.keys()
 
 df = pd.DataFrame(rows, columns=columns)
-
+df['soma'] = df['soma'].apply(lambda x: f"{x:,.2f}")
 print(tabulate(df, headers='keys', tablefmt='psql'))
+
 with conn.connect() as connection:
-    t = text('SELECT status, sum(amount) as soma FROM transactions Group By status ORDER BY status;')    
+    t = text('SELECT currency as moeda, status, sum(amount) as soma FROM transactions Group By moeda, status ORDER BY status;')    
     result = connection.execute(t)	
     rows = result.fetchall()
     columns = result.keys()
@@ -30,6 +31,16 @@ df = pd.DataFrame(rows, columns=columns)
 
 df['soma'] = df['soma'].apply(lambda x: f"{x:,.2f}")
 
+print(tabulate(df, headers='keys', tablefmt='psql'))
+
+with conn.connect() as connection:
+    t = text('SELECT currency as moeda, category as categoria, sum(amount) as soma FROM transactions Group By moeda, categoria ORDER BY categoria;')    
+    result = connection.execute(t)	
+    rows = result.fetchall()
+    columns = result.keys()
+
+df = pd.DataFrame(rows, columns=columns)
+df['soma'] = df['soma'].apply(lambda x: f"{x:,.2f}")
 print(tabulate(df, headers='keys', tablefmt='psql'))
 
 
